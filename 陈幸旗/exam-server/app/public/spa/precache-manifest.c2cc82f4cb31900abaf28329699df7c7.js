3874ff5895e438ed677a4cc9cd6f6df7e27cca4f
chenxingqi03 <3180390539@qq.com>
2021-01-27 11:46:03 +0800

    1

---
+++ 陈幸旗/exam-server/app/public/spa/precache-manifest.c2cc82f4cb31900abaf28329699df7c7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "06f085a8dc89cdddf12f4aad5f999447",
+    "url": "/public/spa/index.html"
+  },
+  {
+    "revision": "4cf77b349f542ebad8f8",
+    "url": "/public/spa/static/css/2.44309f2b.chunk.css"
+  },
+  {
+    "revision": "8941d5f3bf48679e1e3d",
+    "url": "/public/spa/static/css/main.5adb17d0.chunk.css"
+  },
+  {
+    "revision": "4cf77b349f542ebad8f8",
+    "url": "/public/spa/static/js/2.3895c5c3.chunk.js"
+  },
+  {
+    "revision": "ab690bf014f1531643fa2d35ced5342a",
+    "url": "/public/spa/static/js/2.3895c5c3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8941d5f3bf48679e1e3d",
+    "url": "/public/spa/static/js/main.a30e6388.chunk.js"
+  },
+  {
+    "revision": "6121d46de40d09c07c6a",
+    "url": "/public/spa/static/js/runtime-main.c791ec51.js"
+  }
+]);
